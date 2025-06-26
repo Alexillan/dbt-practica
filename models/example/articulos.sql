566d241dde0a69b1181943e5ccf516048e364aa8
Alex Illan <alex.illan-pardo@capgemini.com>
2025-06-26 16:03:31 +0000

    c

--- models/example/articulos.sql
+++ models/example/articulos.sql
@@ -1,1 +1,13 @@
-select * from 'proyecto-dbt-463511.Dataset_practica_dbt.Articulos'
+
+
+{{ config(materialized='table') }}
+
+with articulos as (
+
+    select *
+    from 'proyecto-dbt-463511.Dataset_practica_dbt.Articulos'
+
+)
+
+select *
+from 'proyecto-dbt-463511.Dataset_practica_dbt.Articulos'
